6948d0510ce4c9c9342ce73311c9081b4f66f420
zRhyan <brygamerbr@gmail.com>
2025-12-26 08:25:26 -0300

    Deixe a identacao correta, Pietro!

--- sessoes.c
+++ sessoes.c
@@ -50,84 +50,84 @@ void listarSessoes(Sessoes *sessao, int quantidadeSessoes, Usuarios *usuario, in
         char resposta;
 
         do{
-        printf("\nDeseja comprar um ingresso? (S/N): ");
-        if (scanf(" %c", &resposta) != 1) {
-            resposta = ' '; // Define como inválido se o scanf falhar
-        }
-
-        // Limpa o buffer de entrada para a próxima iteração
-        while (getchar() != '\n'); 
-
-        resposta = toupper(resposta); // Converte para maiúsculo
-
-        if (resposta != 'S' && resposta != 'N') {
-            printf("\nOpcao invalida! Digite S ou N.\n Pressione Enter para tentar novamente.");
-            while (getchar() != '\n'); // Limpa o buffer (caso tenha sobrado algo)
-            getchar(); // Aguarda o usuário pressionar Enter
-        }
-
-    } while (resposta != 'S' && resposta != 'N');
-
-    if(resposta=='S'){
-        int id;
-        int indiceSessao;
-        int linhaAssento;
-        int colunaAssento;
-
-        printf("\nDigite o id do filme: ");
-        
-        do{
-            scanf("%d", &id);
-
-            indiceSessao = buscaSessao(sessao, id, quantidadeSessoes);
-        
-            if(indiceSessao==-1){
-                printf("\nId não localizado, digite novamente.\nDigite o id do filme: ");
-            } else if(usuario[indiceUsuario].idade < sessao[indiceSessao].limIdade){
-                printf("\nIdade não permitida para este filme, tente novamente.\nDigite o id do filme: ");
-                indiceSessao=-1;
-            } else if(usuario[indiceUsuario].saldo < sessao[indiceSessao].valorIngresso){
-                printf("\nNão foi possível adquirir o ingresso, valor insuficiente. Tente novamente.\nDigite o id do filme: ");
-                indiceSessao=-1;
+            printf("\nDeseja comprar um ingresso? (S/N): ");
+            if (scanf(" %c", &resposta) != 1) {
+                resposta = ' '; // Define como inválido se o scanf falhar
             }
-        }while(indiceSessao==-1);
 
-        do{
-            printf("\nDigite a linha do assento desejado (Ex.1): ");
-            scanf("%d", &linhaAssento);
-            linhaAssento -= 1;
+            // Limpa o buffer de entrada para a próxima iteração
+            while (getchar() != '\n'); 
 
-            printf("\nDigite a coluna do assento desejado (Ex.1): ");
-            scanf("%d", &colunaAssento);
-            colunaAssento -= 1;
+            resposta = toupper(resposta); // Converte para maiúsculo
 
-            if(sessao[indiceSessao].assento[linhaAssento][colunaAssento]=='X'){ 
-                printf("Sentimos muito, este assento está ocupado...\n");
+            if (resposta != 'S' && resposta != 'N') {
+                printf("\nOpcao invalida! Digite S ou N.\n Pressione Enter para tentar novamente.");
+                while (getchar() != '\n'); // Limpa o buffer (caso tenha sobrado algo)
+                getchar(); // Aguarda o usuário pressionar Enter
             }
-            //Assentos com o char "O" estarão livres, e os com "X", ocupados;
-        } while (sessao[indiceSessao].assento[linhaAssento][colunaAssento]=='X');
 
-        usuario[indiceUsuario].saldo -= sessao[indiceSessao].valorIngresso; // Dando baixa no saldo do usuário;
+        } while (resposta != 'S' && resposta != 'N');
 
-        reserva[*qtdReservas].id = *qtdReservas;
-        reserva[*qtdReservas].id_sessao = sessao[indiceSessao].id;
-        strcpy(reserva[*qtdReservas].cpf_usuario,usuario[indiceUsuario].cpf);
+        if(resposta=='S'){
+            int id;
+            int indiceSessao;
+            int linhaAssento;
+            int colunaAssento;
 
-        //reserva[*qtdReservas].assento = ??? Devemos analisar depois como atribuir...
-        
-        *qtdReservas++;
+            printf("\nDigite o id do filme: ");
+            
+            do{
+                scanf("%d", &id);
 
-        limparTela();
-        printf("==============================================\n");
-        printf("      Ingresso adquirido com sucesso!");
-        printf("\n==============================================\n");
-        printf("\n[Enter] para retornar ao menu login...");
-        getchar(); // Aguarda o usuário enviar o enter
+                indiceSessao = buscaSessao(sessao, id, quantidadeSessoes);
+            
+                if(indiceSessao==-1){
+                    printf("\nId não localizado, digite novamente.\nDigite o id do filme: ");
+                } else if(usuario[indiceUsuario].idade < sessao[indiceSessao].limIdade){
+                    printf("\nIdade não permitida para este filme, tente novamente.\nDigite o id do filme: ");
+                    indiceSessao=-1;
+                } else if(usuario[indiceUsuario].saldo < sessao[indiceSessao].valorIngresso){
+                    printf("\nNão foi possível adquirir o ingresso, valor insuficiente. Tente novamente.\nDigite o id do filme: ");
+                    indiceSessao=-1;
+                }
+            }while(indiceSessao==-1);
 
-    } else {
-        limparTela();
-        printf("\n[Enter] para retornar ao menu login...");
-        getchar(); // Aguarda o usuário enviar o enter
-    }
+            do{
+                printf("\nDigite a linha do assento desejado (Ex.1): ");
+                scanf("%d", &linhaAssento);
+                linhaAssento -= 1;
+
+                printf("\nDigite a coluna do assento desejado (Ex.1): ");
+                scanf("%d", &colunaAssento);
+                colunaAssento -= 1;
+
+                if(sessao[indiceSessao].assento[linhaAssento][colunaAssento]=='X'){ 
+                    printf("Sentimos muito, este assento está ocupado...\n");
+                }
+                //Assentos com o char "O" estarão livres, e os com "X", ocupados;
+            } while (sessao[indiceSessao].assento[linhaAssento][colunaAssento]=='X');
+
+            usuario[indiceUsuario].saldo -= sessao[indiceSessao].valorIngresso; // Dando baixa no saldo do usuário;
+
+            reserva[*qtdReservas].id = *qtdReservas;
+            reserva[*qtdReservas].id_sessao = sessao[indiceSessao].id;
+            strcpy(reserva[*qtdReservas].cpf_usuario,usuario[indiceUsuario].cpf);
+
+            //reserva[*qtdReservas].assento = ??? Devemos analisar depois como atribuir...
+            
+            *qtdReservas++;
+
+            limparTela();
+            printf("==============================================\n");
+            printf("      Ingresso adquirido com sucesso!");
+            printf("\n==============================================\n");
+            printf("\n[Enter] para retornar ao menu login...");
+            getchar(); // Aguarda o usuário enviar o enter
+
+        } else {
+            limparTela();
+            printf("\n[Enter] para retornar ao menu login...");
+            getchar(); // Aguarda o usuário enviar o enter
+        }
 
 }
