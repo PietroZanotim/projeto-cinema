3d369ca651b74420bf44deb4f4cb1f993838fb56
zRhyan <brygamerbr@gmail.com>
2025-12-26 08:35:01 -0300

    Criacao dos arquivos para as funcoes do admin

---
+++ admin.h
@@ -0,0 +1,12 @@
+//--------------------------!! SOBRE O ARQUIVO !!------------------------------
+//---------------------------{ FUNCOES DO ADMIN }------------------------------
+//-----------------------------------------------------------------------------
+
+#ifndef ADMIN_H  // Guardas de inclusão (evita erro de redefinição)
+#define ADMIN_H
+
+#include "structs.h" // Precisa conhecer a struct Usuarios
+
+
+
+#endif
