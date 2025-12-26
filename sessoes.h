c4bc77e700193fe8bd835ad90f809c89ffd7d746
zRhyan <brygamerbr@gmail.com>
2025-12-26 07:32:14 -0300

    Inclui bibliotecas faltantes

--- sessoes.h
+++ sessoes.h
@@ -1,6 +1,8 @@
 #ifndef SESSOES_H  // Guardas de inclusão (evita erro de redefinição)
 #define SESSOES_H
 
+#include "structs.h" // Precisa conhecer a struct Usuarios
+
 void listarSessoes(Sessoes *sessao, int quantidadeSessoes, Usuarios *usuario, int indiceUsuario, Reservas *reserva, int *qtdReservas);
 int buscaSessao(Sessoes *sessao,int id, int qtdSessoes);
 
