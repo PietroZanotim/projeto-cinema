9db8cbaf2ef12d826e75c493eddbae4546a173cf
Leticia Moura <leti.mouralemos@gmail.com>
2026-01-14 15:42:01 -0300

    Finalizando structs

--- CRUD_sessoes.h
+++ CRUD_sessoes.h
@@ -9,10 +9,10 @@
 
 // --- CRUD SESSÃO (Estrutura 1) ---
 int menu_CRUD_sessoes();
-void adicionar_sessao();
-void excluir_sessao();
-void modificar_sessao();
+void adicionar_sessao(Sessoes **lista, int *qtdSessoes, int *maxSessoes);
+void excluir_sessao(Sessoes *lista, int *qtdSessoes, Reservas *ListaReservas, int qtdREservas);
+void modificar_sessao(Sessoes *lista, int qtdSessoes);
+int buscaSessao(Sessoes *lista, int id, int qtdSessoes); // Função auxiliar (busca e retorna o id do elemento)
 // Nota: Visualizar Sessão está em outro arquivo.
-
 
 #endif
